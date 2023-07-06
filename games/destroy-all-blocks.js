5d1c1d36043be22e8c2613cb9db8b60c461299d9
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2023-07-06 18:04:14 -0400

    updatesss

--- games/destroy-all-blocks.js
+++ games/destroy-all-blocks.js
@@ -6,72 +6,97 @@ kaboom({
   background: [51, 204, 255],
 });
 
-
 loadSprite("bean", "https://kaboomjs.com/sprites/bean.png");
 loadSprite("watermelon", "https://kaboomjs.com/sprites/watermelon.png");
 
 const block_size = 50;
 
 scene("start", () => {
-    let clicks = 5;
-    
-    let getText = () => `Tap ${clicks} times to start again`;
-    
-    let titleText = add([
-        text(getText(), { size: 14 }),
-        pos(center()),
-        anchor("center"),
-    ]);
-    
-    function handleClick() {
-        clicks--;
-        titleText.text = getText();
-        if (clicks === 0) {
-        go("game");
-        }
+  let clicks = 5;
+
+  let getText = () => `Tap ${clicks} times to start again`;
+
+  let titleText = add([
+    text(getText(), { size: 14 }),
+    pos(center()),
+    anchor("center"),
+  ]);
+
+  function handleClick() {
+    clicks--;
+    titleText.text = getText();
+    if (clicks === 0) {
+      go("game", 0);
     }
-    onClick(handleClick);
-    onKeyPress(handleClick);
+  }
+  onClick(handleClick);
+  onKeyPress(handleClick);
 });
 
-scene("game", () => {
+let levels = [
+  [
+    "==========================",
+    "L************************R",
+    "L************************R",
+    "L************************R",
+    "L************************R",
+    "L************************R",
+    "L************************R",
+    "L    B                   R",
+    "==========================",
+  ],
+  [
+    "==========================",
+    "L**********L*************R",
+    "L**********L*************R",
+    "L**********L*************R",
+    "L********** *************R",
+    "L********** *************R",
+    "L**********L*************R",
+    "L    B     L             R",
+    "==========================",
+  ],
+];
 
+scene("game", (levelIdx = 0) => {
   setGravity(3000);
-
-  let level = addLevel(
-    [
-      "==========================",
-      "=************************=",
-      "=************************=",
-      "=************************=",
-      "=************************=",
-      "=************************=",
-      "=************************=",
-      "=    B                   =",
-      "==========================",
-    ],
-    {
-      tileHeight: block_size,
-      tileWidth: block_size,
-      tiles: {
-        "=": () => [
-          rect(block_size, block_size),
-          color(100, 100, 100),
-          area(),
-          body({ isStatic: true }),
-          "wall",
-        ],
-        B: () => [
-          sprite("bean"),
-          area(),
-          offscreen({ destroy: false }),
-          body(),
-          "bean",
-        ],
-        "*": () => [sprite("watermelon"), area(), "watermelon"],
-      },
-    }
-  );
+  console.log("level", levelIdx);
+  let isLastLevel = levelIdx === levels.length - 1;
+  let level = addLevel(levels[levelIdx], {
+    tileHeight: block_size,
+    tileWidth: block_size,
+    tiles: {
+      "=": () => [
+        rect(block_size, block_size),
+        color(100, 100, 100),
+        area(),
+        body({ isStatic: true }),
+        "wall",
+      ],
+      L: () => [
+        rect(block_size, block_size),
+        color(100, 100, 100),
+        area(),
+        body({ isStatic: true }),
+        "left-wall",
+      ],
+      R: () => [
+        rect(block_size, block_size),
+        color(100, 100, 100),
+        area(),
+        body({ isStatic: true }),
+        "right-wall",
+      ],
+      B: () => [
+        sprite("bean"),
+        area(),
+        offscreen({ destroy: false }),
+        body(),
+        "bean",
+      ],
+      "*": () => [sprite("watermelon"), area(), "watermelon"],
+    },
+  });
 
   let bean = level.get("bean")[0];
 
@@ -90,17 +115,34 @@ scene("game", () => {
 
   onCollide("bean", "watermelon", (bean, watermelon) => {
     destroy(watermelon);
-    addNotificationText("+1", watermelon.pos);
-    if (level.get("watermelon").length === 0) {
-      go("start");
+    let remaining = level.get("watermelon").length;
+
+    if (remaining > 0) {
+      addNotificationText("+1", watermelon.pos);
+    } else {
+      addNotificationText("You win!", watermelon.pos);
+      wait(1, () => {
+        if (isLastLevel) {
+        //   go("start");
+        } else {
+          go("game", levelIdx + 1);
+        }
+      });
     }
+  });
+
+  onCollide("bean", "left-wall", (bean) => {
+    direction = 1;
+  });
+  onCollide("bean", "right-wall", (bean) => {
+    direction = -1;
   });
 
   // add a kaboom on mouse click
   onClick(() => {
     let { x } = mousePos();
     let beanX = toScreen(bean.pos).x;
-    debug.log(`x: ${x}, beanX: ${beanX}`)
+    debug.log(`x: ${x}, beanX: ${beanX}`);
     if (x < beanX) {
       direction = -1;
     } else {
