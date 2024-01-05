01728df3125b89a5f9b177181c85be5ec126895e
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2024-01-05 01:54:32 -0700

    add block dude

--- games/block-dude.js
+++ games/block-dude.js
@@ -53,8 +53,16 @@ function resetTo(level = 0) {
     .split("\n")
     .filter((row) => row.length > 0)
     .map((row) => row.split(""));
-  manCoords = undefined;
-  doorCoords = undefined;
+  for (let y = 0; y < rows.length; y++) {
+    for (let x = 0; x < rows[y].length; x++) {
+      if (rows[y][x] === "M") {
+        manCoords = { x, y, dir: 1 };
+      }
+      if (rows[y][x] === "E") {
+        doorCoords = { x, y };
+      }
+    }
+  }
 }
 
 resetTo(0);
@@ -68,12 +76,16 @@ window.addEventListener("resize", () => {
   requestAnimationFrame(paint);
 });
 
-window.addEventListener("keydown", (event) => {
+/**
+ *
+ * @param {"left" | "right" | "pickup"} action
+ */
+function onInput(action) {
   if (!manCoords) {
     return;
   }
 
-  if (event.key === "ArrowLeft") {
+  if (action === "left") {
     if (manCoords.dir === 1) {
       manCoords.dir = -1;
       paint();
@@ -81,7 +93,7 @@ window.addEventListener("keydown", (event) => {
     }
     moveMan(-1);
   }
-  if (event.key === "ArrowRight") {
+  if (action === "right") {
     if (manCoords.dir === -1) {
       manCoords.dir = 1;
       paint();
@@ -89,37 +101,99 @@ window.addEventListener("keydown", (event) => {
     }
     moveMan(1);
   }
-  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+  if (action === "pickup") {
     if (canPickUpBlock()) {
-      rows[manCoords.y][manCoords.x] = "B";
-      manCoords.y++;
-      rows[manCoords.y][manCoords.x] = "M";
+      const blockCoords = getPickUpBlockCoords();
+      rows[blockCoords.y][blockCoords.x] = " ";
+      rows[manCoords.y - 1][manCoords.x] = "B";
       paint();
     } else if (hasBlock()) {
-      rows[manCoords.y][manCoords.x] = "B";
-      manCoords.y--;
-      rows[manCoords.y][manCoords.x] = "M";
+      const blockCoords = getPlaceBlockCoords();
+      rows[blockCoords.y][blockCoords.x] = "B";
+      rows[manCoords.y - 1][manCoords.x] = " ";
       paint();
     }
   }
+}
+
+window.addEventListener("keydown", (event) => {
+  let keyToAction = {
+    ArrowLeft: "left",
+    ArrowRight: "right",
+    ArrowUp: "pickup",
+    ArrowDown: "pickup",
+  };
+
+  let action = keyToAction[event.key];
+  if (action) {
+    onInput(action);
+  }
+});
+
+window.addEventListener('touchmove', (event) => {
+    onInput('pickup');
+})
+
+window.addEventListener("click", (event) => {
+    // click on left side of window
+    if (event.clientX < window.innerWidth / 2) {
+        onInput('left');
+    } else {
+        onInput('right');
+    }
 });
 
 function isOpenSpace(x, y) {
-  return rows[y][x] === " " || rows[y][x] === "M";
+  return rows[y][x] === " " || rows[y][x] === "M" || rows[y][x] === "E";
 }
 function isSolidSpace(x, y) {
   return rows[y][x] === "=" || rows[y][x] === "B";
 }
 
-function canPickUpBlock() {
+/**
+ * @returns {Coord|null}
+ */
+function getPickUpBlockCoords() {
   if (hasBlock()) {
-    return false;
+    return null;
   }
   if (!manCoords) {
-    return false;
+    return null;
+  }
+  let blockX = manCoords.x + manCoords.dir;
+  let blockY = manCoords.y - 1;
+  while (blockY <= manCoords.y + 1) {
+    if (rows[blockY][blockX] === "B" && rows[blockY - 1][blockX] !== "B") {
+      return { x: blockX, y: blockY };
+    }
+    blockY++;
   }
 
-  return rows[manCoords.y + 1][manCoords.x] === "B";
+  return null;
+}
+
+/**
+ * @returns {Coord|null}
+ */
+function getPlaceBlockCoords() {
+  if (!hasBlock()) {
+    return null;
+  }
+  if (!manCoords) {
+    return null;
+  }
+  let blockX = manCoords.x + manCoords.dir;
+  let blockY = manCoords.y;
+  while (blockY < rows.length) {
+    if (rows[blockY][blockX] === " " && isSolidSpace(blockX, blockY + 1)) {
+      return { x: blockX, y: blockY };
+    }
+    blockY++;
+  }
+}
+
+function canPickUpBlock() {
+  return !!getPickUpBlockCoords();
 }
 
 function hasBlock() {
@@ -161,13 +235,83 @@ function moveMan(dx) {
 
 function _paint() {
   const ctx = canvas.getContext("2d");
-  const blockSize = window.innerWidth / rows[0].length;
-  console.log("blockSize", blockSize);
-  canvas.width = window.innerWidth;
-  canvas.height = blockSize * rows.length;
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "skyblue";
   console.log("manDir", manCoords?.dir);
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const blockSize = Math.min(window.innerWidth, window.innerHeight) / 9;
+  console.log("blockSize", blockSize);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  let xBlocksInView = Math.ceil(window.innerWidth / blockSize);
+  let YBlocksInView = Math.ceil(window.innerHeight / blockSize);
+  let dy = Math.round(YBlocksInView / 2);
+  let dx = Math.round(xBlocksInView / 2);
+  let rootX = manCoords.x - dx;
+  let rootY = manCoords.y - dy;
+
+  for (let _y = 0; _y < YBlocksInView; _y++) {
+    for (let _x = 0; _x < xBlocksInView; _x++) {
+      let x = rootX + _x;
+      let y = rootY + _y;
+      if (x < 0 || y < 0) {
+        continue;
+      }
+      const char = rows[y][x];
+      console.log("char", { manCoords, x, y, char });
+
+      if (doorCoords?.x === x && doorCoords?.y === y) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(_x * blockSize, _y * blockSize, blockSize, blockSize);
+      }
+      if (char === "B") {
+        ctx.fillStyle = "purple";
+        ctx.fillRect(_x * blockSize, _y * blockSize, blockSize, blockSize);
+      }
+
+      if (char === "=") {
+        ctx.fillStyle = "green";
+        ctx.fillRect(_x * blockSize, _y * blockSize, blockSize, blockSize);
+      }
+
+      if (manCoords?.x === x && manCoords?.y === y) {
+        ctx.fillStyle = "pink";
+        ctx.fillRect(_x * blockSize, _y * blockSize, blockSize, blockSize);
+        // add hair
+        ctx.fillStyle = "brown";
+        ctx.fillRect(_x * blockSize, _y * blockSize, blockSize, blockSize / 4);
+        // add eye
+        ctx.fillStyle = "black";
+        const eyeWidth = blockSize / 8;
+        const eyeX =
+          manCoords.dir === -1
+            ? _x * blockSize
+            : _x * blockSize + blockSize - eyeWidth;
+        ctx.fillRect(
+          eyeX,
+          _y * blockSize + blockSize * 0.4,
+          blockSize / 8,
+          blockSize / 8
+        );
+
+        // add mouth
+        const mouthWidth = blockSize / 2;
+        const mouthX =
+          manCoords.dir === -1
+            ? _x * blockSize
+            : _x * blockSize + blockSize - mouthWidth;
+        ctx.fillRect(
+          mouthX,
+          _y * blockSize + blockSize * 0.8,
+          mouthWidth,
+          blockSize / 16
+        );
+      }
+    }
+  }
+
+  return;
   for (let y = 0; y < rows.length; y++) {
     const row = rows[y];
     for (let x = 0; x < row.length; x++) {
