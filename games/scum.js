fdd7013451c24cca432523cf6ff8a63d52686906
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2024-01-20 00:10:58 -0800

    making progress

--- games/scum.js
+++ games/scum.js
@@ -1,4 +1,76 @@
-import { html, render } from "https://cdn.jsdelivr.net/npm/lit-html@3.0.2/lit-html.min.js";
+import van from "../deps/van.js";
+import { reactive, list, calc } from "../deps/van-x.js";
+import { div, p, tr } from "./utils/tags.js";
+import { css } from "./utils/css.js";
+import { randomItem } from "./utils/random.js";
+console.log("here");
+
+const windowWidth = van.state(window.innerWidth);
+const windowHeight = van.state(window.innerHeight);
+window.addEventListener("resize", () => {
+  windowWidth.val = window.innerWidth;
+  windowHeight.val = window.innerHeight;
+});
+
+/**
+ *
+ * @param {Card} c
+ */
+function getKey(c) {
+  return c.suit + c.value;
+}
+
+/**
+ *
+ * @returns {Card[]}
+ */
+function genCards() {
+  const cards = [];
+  /** @type {Card['suit'][]} */
+  const suits = ["spades", "hearts", "clubs", "diamonds"];
+  for (const suit of suits) {
+    for (let i = 1; i <= 13; i++) {
+      cards.push({
+        suit,
+        value: i,
+      });
+    }
+  }
+  // @ts-ignore
+  return cards;
+}
+
+const state = reactive(
+  /** @type {State} */
+  ({
+    players: [],
+    cards: genCards().reduce((acc, c) => {
+      acc[getKey(c)] = {
+        ...c,
+      };
+      return acc;
+    }, {}),
+  })
+);
+
+const transforms = reactive(
+  /** @type {Record<string, { x: number, y: number }>} */
+  (
+    genCards().reduce((acc, c) => {
+      acc[getKey(c)] = {
+        x: 0,
+        y: 0,
+      };
+      return acc;
+    }, {})
+  )
+);
+
+const local = reactive({
+  hand: calc(() => Object.values(state.cards)),
+  discardPile: calc(() => Object.values(state.cards)),
+});
+
 console.log("game slot", document.getElementById("game-slot"));
 /**
  * @typedef Card
@@ -11,13 +83,11 @@ console.log("game slot", document.getElementById("game-slot"));
  * @typedef Player
  * @type {{
  *   name: string,
- *   hand: Card[],
  * }}
  *
  * @typedef {{
  *  players: Player[],
- *  deck: Card[],
- *  discard: Card[],
+ *  cards: Record<string, Card>,
  * }} State
  *
  * @typedef {{
@@ -48,6 +118,14 @@ function suitToSymbol(suit) {
   }
 }
 
+setInterval(() => {
+  const randomKey = randomItem(Object.keys(state.cards));
+  transforms[randomKey] = {
+    x: Math.random() * windowWidth.val - 100,
+    y: Math.random() * windowHeight.val - 100,
+  };
+}, 100);
+
 /**
  *
  * @param {Card['value']} value
@@ -73,49 +151,91 @@ function valueToCharacter(value) {
  * @returns
  */
 function Card(c, covered = false) {
-  return html`
-    <div class="card ${c.suit} ${covered ? 'covered' : ''}" draggable="true">
-      <p>${valueToCharacter(c.value)}</p>
-      <p>${suitToSymbol(c.suit)}</p>
-    </div>
-  `;
+  return div(
+    {
+      class: () => `card ${c.suit} ${covered ? "" : "revealed"}`,
+      style: () => {
+        console.log("update style!");
+        const transform = transforms[c.suit + c.value];
+        if (!transform) {
+          return "";
+        }
+        return `transform: translate(${transform.x}px, ${transform.y}px)`;
+      },
+    },
+    p(valueToCharacter(c.value)),
+    p(suitToSymbol(c.suit))
+  );
 }
 
 const myHand = document.querySelector("#my-hand");
 const pile = document.querySelector("#pile");
 
 console.log({
-    pile,
-    myHand
-})
+  pile,
+  myHand,
+});
 
-render(
-    html`
-    ${Card({ suit: "spades", value: 1 })}
-    ${Card({ suit: "hearts", value: 1 })}
-    ${Card({ suit: "clubs", value: 1 })}
-    ${Card({ suit: "diamonds", value: 1 })}
-    `,
-    pile
+van.add(
+  document.getElementById("game-slot"),
+  list(
+    () => div({ id: "discard-pile", class: "pile" }),
+    local.discardPile,
+    (c) => Card(c.val, true)
+  ),
+  list(
+    () => div({ id: "my-hand", class: "hand" }),
+    local.hand,
+    (c) => Card(c.val)
+  )
+);
+css`
+  .typewriter-cursor {
+    animation: blink 1s infinite;
+    font-weight: bold;
+    font-size: 1.2em;
+    color: #333;
+    margin-left: 4px;
+  }
 
-)
+  @keyframes blink {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
 
-render(
-    html`
-    ${Card({ suit: "spades", value: 1 })}
-    ${Card({ suit: "hearts", value: 1 })}
-    ${Card({ suit: "clubs", value: 1 })}
-    ${Card({ suit: "diamonds", value: 1 })}
-    `,
-    myHand
-)
+function getTypewriterAction(currentValue, targetValue) {
+  if (currentValue === targetValue) {
+    return { action: "noop" };
+  }
+  if (currentValue.length > targetValue.length) {
+    return { action: "delete" };
+  }
+  let mismatchIndex = -1;
+  for (let i = 0; i < currentValue.length; i++) {
+    if (currentValue[i] !== targetValue[i]) {
+      mismatchIndex = i;
+      break;
+    }
+  }
 
-render(
-        html`
-        ${Card({ suit: "spades", value: 1 })}
-        ${Card({ suit: "hearts", value: 1 })}
-        ${Card({ suit: "clubs", value: 1 })}
-        ${Card({ suit: "diamonds", value: 1 })}
-        `,
-        document.getElementById('hand-2')
-)
+  if (mismatchIndex === -1) {
+    return { action: "add", value: targetValue[currentValue.length] };
+  }
+
+  if (mismatchIndex < currentValue.length) {
+    return { action: "delete" };
+  }
+
+  return { action: "add", value: targetValue[currentValue.length] };
+}
+// Array.from(document.querySelectorAll('code[data-class] > div[data-tag] > span[data-id] > i')).map(n => n.getAttribute('value')).join('')
+// https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/616e67'
+// angelic
