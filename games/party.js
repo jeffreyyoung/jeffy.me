3400467d95ca694a69f658fb44efc09aa8a2b918
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2024-01-30 21:03:50 -0800

    fix nested scroll bars

--- games/party.js
+++ games/party.js
@@ -327,8 +327,10 @@ van.add(
       () =>
         mainViewContents.val === "in-game"
           ? iframe({
+              id: "game-iframe",
               src: iframeUrl,
-              style: "border: none; width: 100%; height: 100%;",
+              style:
+                "border: none; width: 100%; height: 100%; top: 0; left: 0; bottom: 0; right: 0; position: absolute;",
             })
           : div()
     )
