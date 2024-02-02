1d01e72a5f84c8e34b3c3fa9a9f22b5d10c7b959
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2024-02-01 20:53:20 -0800

    updates

---
+++ games/embeds/dudes.js
@@ -0,0 +1,3 @@
+import * as THREE from "https://esm.sh/three@0.161.0";
+
+console.log("here");
--- games/embeds/embed.js
+++ games/embeds/embed.js
@@ -2,7 +2,6 @@ import { games } from "../game-index.js";
 import { getQueryParam } from "../utils/random.js";
 
 const game = getQueryParam("game");
-
-if (games.find((g) => g.url === game)) {
+if (games.find((g) => g.url === game) || game.startsWith("/games/")) {
   import(game);
 }
